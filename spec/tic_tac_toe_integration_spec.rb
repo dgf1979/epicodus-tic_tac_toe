d39ce7caf95f0153f590af948ddc183011b8ad8d
Drew & Toby Alden <student@epicodus.com>
2015-04-30 15:37:05 -0700

    Adds integration specs. Specs pass.

--- spec/tic_tac_toe_integration_spec.rb
+++ spec/tic_tac_toe_integration_spec.rb
@@ -15,4 +15,29 @@ describe("tic tac toe game", {:type => :feature}) do
     visit('/')
     expect(page).to have_content("Welcome")
   end
+
+  it('can restart with a fresh game', { :type => :feature}) do
+    visit('/')
+    click_button("new_game")
+    expect(page).to have_content("Welcome")
+    click_link('00')
+    expect(page).to have_content("O's Turn")
+  end
+
+  it('plays a turn', { :type => :feature}) do
+    visit('/')
+    click_link('00')
+    expect(page).to have_content("O's Turn")
+  end
+
+  it('plays a whole game', { :type => :feature}) do
+    visit('/')
+    click_link('00')
+    click_link('10')
+    click_link('01')
+    click_link('20')
+    click_link('02')
+    expect(page).to have_content("The winner is: X!")
+  end
+
 end
